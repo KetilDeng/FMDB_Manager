0fe078613ac98604a911a4c0ca50e734379edca0
Ketil <dkts47@163.com>
2018-05-26 17:19:19 +0800

    整理修改于2018/5/26

--- FMDB_Manager/Tool/FMDBManager.h
+++ FMDB_Manager/Tool/FMDBManager.h
@@ -8,12 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-#import <FMDB/FMDB.h>
-
 @interface FMDBManager : NSObject
-
-@property (nonatomic, strong) FMDatabase *database;
-@property (nonatomic, strong) FMDatabaseQueue *databaseQueue;
 
 + (void)insertWithModel:(id)model;
 + (void)insertWithModel:(id)model andWithSuffix:(NSString *)suffix;
@@ -25,72 +20,8 @@
 + (void)deleteTableWithModel:(id)model;
 + (void)deleteTableWithModel:(id)model andWithSuffix:(NSString *)suffix;
 + (void)updateWithModel:(id)model andWithSuffix:(NSString *)suffix andKey:(NSString *)key andValue:(NSString *)value;
-
-#if 0
-
-/*!
- *  @brief 创建表
- *
- *  @param model  表名
- *  @param suffix 表名后缀（公用model需要后缀来标识为不同表）
- */
--(void)creatTable:(id)model andWithSuffix:(NSString *)suffix;
-
-/*!
- *  @brief 插入、更新表
- *
- *  @param model  表名
- *  @param suffix 表名后缀
- */
--(void)insertAndUpdateModelToDatabase:(id)model andWithSuffix:(NSString *)suffix;
-
-/*!
- *  @brief 插入、更新表
- *
- *  @param model  表名
- *  @param suffix 表名后缀
- *  @param key    插入、更新的key
- *  @param value  插入、更新的值
- */
--(void)insertAndUpdateModelToDatabase:(id)model andWithSuffix:(NSString *)suffix andKey:(NSString *)key andValue:(NSString *)value;
-
-/*!
- *  @brief 根据键值对删除表中数据
- *
- *  @param model  表名
- *  @param suffix 表名后缀
- *  @param key    删除的key
- *  @param value  删除的值
- */
--(void)deleteModelInDatabase:(id)model andWithSuffix:(NSString *)suffix andKey:(NSString *)key andValue:(NSString *)value;
-
-/*!
- *  @brief 删除表
- *
- *  @param model  表名
- *  @param suffix 表名后缀
- */
-- (void)deleteTable:(id)model andWithSuffix:(NSString *)suffix;
-
-/*!
- *  @brief 查询表中所有数据
- *
- *  @param model  表名
- *  @param suffix 表名后缀
- *
- *  @return 查询结果
- */
-- (NSArray *)selectModelArrayInDatabase:(id)model andWithSuffix:(NSString *)suffix;
-#endif
-
-
-/*!
- *  @brief 获取model的属性名
- *
- *  @param model model
- *
- *  @return 获取结果
- */
-+ (NSArray *)getAllProperties:(id)model;
++ (void)queryModel:(id)model andWithSuffix:(NSString *)suffix completed:(void(^)(NSArray *result))completed;
++ (void)queryModel:(id)model andWithSuffix:(NSString *)suffix key:(NSString *)key value:(NSString *)value completed:(void (^)(NSArray *result))completed;
++ (void)addField:(id)model andWithSuffix:(NSString *)suffix field:(NSString *)field;
 
 @end
--- FMDB_Manager/Tool/FMDBManager.m
+++ FMDB_Manager/Tool/FMDBManager.m
@@ -7,14 +7,21 @@
 //
 
 #import "FMDBManager.h"
+#import <FMDB/FMDB.h>
 #import <objc/runtime.h>
-
 //通过model获取类名
 #define KCLASS_NAME(model) [NSString stringWithUTF8String:object_getClassName(model)]
 //通过model获取属性数组数目
 #define KMODEL_PROPERTYS_COUNT [[self getAllProperties:model] count]
 //通过model获取属性数组
 #define KMODEL_PROPERTYS [self getAllProperties:model]
+
+@interface FMDBManager ()
+
+@property (nonatomic, strong) FMDatabase *database;
+@property (nonatomic, strong) FMDatabaseQueue *databaseQueue;
+
+@end
 
 @implementation FMDBManager
 
@@ -33,10 +40,6 @@ static FMDBManager *_shareInstance = nil;
     return [self sharedInstance];
 }
 
-+ (id)copyWithZone:(struct _NSZone *)zone
-{
-    return _shareInstance;
-}
 
 /** 获取model的属性名 */
 + (NSArray *)getAllProperties:(id)model
@@ -72,6 +75,7 @@ static FMDBManager *_shareInstance = nil;
         [filemanage createDirectoryAtPath:docsdir withIntermediateDirectories:YES attributes:nil error:nil];
     }
     NSString *dbpath = [docsdir stringByAppendingPathComponent:@"db.sqlite"];
+    NSLog(@"数据库地址%@",dbpath);
     return dbpath;
 }
 
@@ -117,9 +121,16 @@ static FMDBManager *_shareInstance = nil;
             // 4.整句创建表语句拼接
             NSString *creatTableStr = [NSString string];
             creatTableStr = [creatTableStr stringByAppendingFormat:@"%@%@%@",creatTableStrHeader,creatTableStrMiddle,creatTableStrTail];
-            [db executeUpdate:creatTableStr];
+            if ([db executeUpdate:creatTableStr])
+            {
+                NSLog(@"创建表成功");
+            }
+            else
+            {
+                NSLog(@"创建表失败");
+            }
         }
-    }];    
+    }];
 }
 
 /** 插入操作 */
@@ -166,11 +177,11 @@ static FMDBManager *_shareInstance = nil;
         insertStr = [insertStr stringByAppendingFormat:@"%@%@%@%@%@",insertStrHeader,insertStrMiddleOne,insertStrMiddleTwo,insertStrMiddleThree,insertStrTail];
         NSMutableArray *modelPropertyArray = [NSMutableArray array];
         for (int i = 0; i < KMODEL_PROPERTYS_COUNT; i++){
-            NSString *str = [model valueForKey:[KMODEL_PROPERTYS objectAtIndex:i]];
-            if (str == nil){
-                str = @"none";
+            id object = [model valueForKey:[KMODEL_PROPERTYS objectAtIndex:i]];
+            if (object == nil){
+                object = @"none";
             }
-            [modelPropertyArray addObject: str];
+            [modelPropertyArray addObject: object];
         }
         BOOL resultbool = [db executeUpdate:insertStr withArgumentsInArray:modelPropertyArray];
         if (resultbool) {
@@ -209,7 +220,14 @@ static FMDBManager *_shareInstance = nil;
         NSString *tableName = suffix?[KCLASS_NAME(model) stringByAppendingString:suffix] : KCLASS_NAME(model);
         //多条件删除
         NSString *sql = [NSString stringWithFormat:@"DELETE FROM %@ %@ ",tableName,criteria];
-        [db executeUpdate:sql];
+        if ([db executeUpdate:sql])
+        {
+            NSLog(@"删除成功");
+        }
+        else
+        {
+            NSLog(@"删除失败");
+        }
     }];
 }
 
@@ -222,12 +240,28 @@ static FMDBManager *_shareInstance = nil;
             //delete from tableName where userId = ?
             //单个条件删除
             NSString *deletStr = [NSString stringWithFormat:@"delete from %@ where %@ = ?",tableName,key];
-            [db executeUpdate:deletStr, value];
-        }else{
+            if ([db executeUpdate:deletStr, value])
+            {
+                NSLog(@"删除成功");
+            }
+            else
+            {
+                NSLog(@"删除失败");
+            }
+        }
+        else
+        {
             //delete from tableName
             //清空表
             NSString *deletStr = [NSString stringWithFormat:@"delete from %@",tableName];
-            [db executeUpdate:deletStr];
+            if ([db executeUpdate:deletStr])
+            {
+                NSLog(@"成功清空表");
+            }
+            else
+            {
+                NSLog(@"清空表失败");
+            }
         }
     }];
 }
@@ -243,13 +277,20 @@ static FMDBManager *_shareInstance = nil;
     FMDBManager *dbManager = [FMDBManager sharedInstance];
     [dbManager.databaseQueue inDatabase:^(FMDatabase *db) {
         NSString *tableName = suffix?[KCLASS_NAME(model) stringByAppendingString:suffix] : KCLASS_NAME(model);
-        if ([db tableExists:tableName]) {
+        if ([db tableExists:tableName])
+        {
             NSString *seletTabelStr = [NSString stringWithFormat:@"drop table %@",tableName];
-            BOOL resultbool = [db executeUpdate:seletTabelStr];
-            if (resultbool) {
+            if ([db executeUpdate:seletTabelStr])
+            {
                 NSLog(@"成功删除表");
             }
-        }else{
+            else
+            {
+                NSLog(@"删除表失败");
+            }
+        }
+        else
+        {
             NSLog(@"%@ 表格不存在",tableName);
         }
     }];
@@ -291,285 +332,94 @@ static FMDBManager *_shareInstance = nil;
             }
             [propertyArray addObject:midStr];
         }
-        [db executeUpdate:updateStr withArgumentsInArray:propertyArray];
+        if ([db executeUpdate:updateStr withArgumentsInArray:propertyArray]) {
+            NSLog(@"更新成功");
+        } else {
+            NSLog(@"更新失败");
+        }
     }];
 }
 
-
-#if 0
-/** 获取沙盒路径 */
-- (NSString *)databaseFilePath
-{
-    NSArray *filePath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *documentPath = [filePath objectAtIndex:0];
-    NSString *dbFilePath = [documentPath stringByAppendingPathComponent:@"db.sqlite"];
-    return dbFilePath;
-}
-
-/** 创建数据库 */
-- (void)creatDatabase
-{
-    _database = [FMDatabase databaseWithPath:[self databaseFilePath]];
-}
-
-/** 创建表 */
-- (void)creatTable:(id)model andWithSuffix:(NSString *)suffix
-{
-    //先判断数据库是否存在，如果不存在，创建数据库
-    if (!_database){
-        [self creatDatabase];
-    }
-    //判断数据库是否已经打开，如果没有打开，提示失败
-    if (![_database open]){
-        NSLog(@"数据库打开失败");
-        return;
-    }
-    
-    //为数据库设置缓存，提高查询效率
-    [_database setShouldCacheStatements:YES];
-    
-    //判断数据库中是否已经存在这个表，如果不存在则创建该表
-    NSString *tableName = suffix?[KCLASS_NAME(model) stringByAppendingString:suffix] : KCLASS_NAME(model);
-    if (![_database tableExists:tableName]){
-        //  create table weibomodel (id integer primary key,userID text,userClass text,screenName text,name text,province text,city text,location text...)
-        //（1）获取类名作为数据库表名
-        //（2）获取类的属性作为数据表字段
-        
-        // 1.创建表语句头部拼接
-        NSString *creatTableStrHeader = [NSString stringWithFormat:@"create table %@(id INTEGER PRIMARY KEY",tableName];
-        // 2.创建表语句中部拼接
-        NSString *creatTableStrMiddle =[NSString string];
-        for (int i = 0; i < KMODEL_PROPERTYS_COUNT; i++)
-        {
-            creatTableStrMiddle = [creatTableStrMiddle stringByAppendingFormat:@",%@ TEXT",[KMODEL_PROPERTYS objectAtIndex:i]];
-        }
-        // 3.创建表语句尾部拼接
-        NSString *creatTableStrTail =[NSString stringWithFormat:@")"];
-        // 4.整句创建表语句拼接
-        NSString *creatTableStr = [NSString string];
-        creatTableStr = [creatTableStr stringByAppendingFormat:@"%@%@%@",creatTableStrHeader,creatTableStrMiddle,creatTableStrTail];
-        [_database executeUpdate:creatTableStr];
-        
-        NSLog(@"创建完成");
-    }
-    //关闭数据库
-    [_database close];
-}
-
-/** 增加或更新 */
-- (void)insertAndUpdateModelToDatabase:(id)model andWithSuffix:(NSString *)suffix
-{
-    [self insertAndUpdateModelToDatabase:model andWithSuffix:suffix andKey:nil andValue:nil];
-}
-
-/** 增加或更新 */
-- (void)insertAndUpdateModelToDatabase:(id)model andWithSuffix:(NSString *)suffix andKey:(NSString *)key andValue:(NSString *)value
-{
-    // 判断数据库是否存在
-    if (!_database){
-        [self creatDatabase];
-    }else{
-        [_database open];
-    }
-    
-    // 判断数据库能否打开
-    if (![_database open]){
-        NSLog(@"数据库打开失败");
-        return;
-    }
-    
-    // 设置数据库缓存
-    [_database setShouldCacheStatements:YES];
-    
-    // 判断是否存在对应的userModel表
-    NSString *tableName = suffix?[KCLASS_NAME(model) stringByAppendingString:suffix] : KCLASS_NAME(model);
-    if(![_database tableExists:tableName]){
-        [self creatTable:model andWithSuffix:suffix];
-    }
-    
-    //以上操作与创建表是做的判断逻辑相同
-    //现在表中查询有没有相同的元素，如果有，做修改操作
-    // select *from tableName where userID = ?
-    
-    // 拼接查询语句头部
-    NSString *selectStrHeader = [NSString stringWithFormat:@"select * from %@ where ",tableName];
-    // 拼接查询语句尾部
-#warning --- 优化：考虑遍历所有属性，因为共用model不一定所有属性都有值
-    NSString *selectStrTail = [NSString stringWithFormat:@"%@ = ?",[KMODEL_PROPERTYS objectAtIndex:0]];
-    // 整个查询语句拼接
-    NSString *selectStr = [NSString string];
-    selectStr = [selectStr stringByAppendingFormat:@"%@%@",selectStrHeader,selectStrTail];
-    FMResultSet * resultSet = [_database executeQuery:selectStr,[model valueForKey:[KMODEL_PROPERTYS objectAtIndex:0]]];
-    
-    if([resultSet next]){
-        // update tableName set userID = ?,userClass = ?,screenName = ?,name = ?,province = ?,city = ?,location = ? where userID = ?
-        
-        // 拼接更新语句的头部
-        NSString *updateStrHeader = [NSString stringWithFormat:@"update %@ set ",tableName];
-        // 拼接更新语句的中部
-        NSString *updateStrMiddle = [NSString string];
-        for (int i = 0; i< KMODEL_PROPERTYS_COUNT; i++)
-        {
-            updateStrMiddle = [updateStrMiddle stringByAppendingFormat:@"%@ = ?",[KMODEL_PROPERTYS objectAtIndex:i]];
-            if (i != KMODEL_PROPERTYS_COUNT -1) {
-                updateStrMiddle = [updateStrMiddle stringByAppendingFormat:@","];
-            }
-        }
-        // 拼接更新语句的尾部
-        NSString *updateStrTail = [NSString stringWithFormat:@" where %@ = %@",key,value];
-        // 整句拼接更新语句
-        NSString *updateStr = [NSString string];
-        updateStr = [updateStr stringByAppendingFormat:@"%@%@%@",updateStrHeader,updateStrMiddle,updateStrTail];
-        
-        NSMutableArray *propertyArray = [NSMutableArray array];
-        
-        for (int i = 0; i < KMODEL_PROPERTYS_COUNT; i++)
-        {
-            NSString *midStr = [model valueForKey:[KMODEL_PROPERTYS objectAtIndex:i]];
-            // 判断属性值是否为空
-            if (midStr == nil)
-            {
-                midStr = @"none";
-            }
-            [propertyArray addObject:midStr];
-        }
-        [_database executeUpdate:updateStr withArgumentsInArray:propertyArray];
-    }
-    //向数据库中插入一条数据
-    else{
-        // 拼接插入语句的头部
-        // insert into tableName ( userID,userClass,screenName,name,province,city,location) values (?,?,?,?,?,?,?)
-        
-        NSString *insertStrHeader = [NSString stringWithFormat:@"INSERT INTO %@ (",tableName];
-        // 拼接插入语句的中部1
-        NSString *insertStrMiddleOne = [NSString string];
-        for (int i = 0; i < KMODEL_PROPERTYS_COUNT; i++){
-            insertStrMiddleOne = [insertStrMiddleOne stringByAppendingFormat:@"%@",[KMODEL_PROPERTYS objectAtIndex:i]];
-            
-            if (i != KMODEL_PROPERTYS_COUNT -1){
-                insertStrMiddleOne = [insertStrMiddleOne stringByAppendingFormat:@","];
-            }
-        }
-        // 拼接插入语句的中部2
-        NSString *insertStrMiddleTwo = [NSString stringWithFormat:@") VALUES ("];
-        // 拼接插入语句的中部3
-        NSString *insertStrMiddleThree = [NSString string];
-        for (int i = 0; i < KMODEL_PROPERTYS_COUNT; i++){
-            
-            insertStrMiddleThree = [insertStrMiddleThree stringByAppendingFormat:@"?"];
-            if (i != KMODEL_PROPERTYS_COUNT-1){
-                insertStrMiddleThree = [insertStrMiddleThree stringByAppendingFormat:@","];
-            }
-        }
-        // 拼接插入语句的尾部
-        NSString *insertStrTail = [NSString stringWithFormat:@")"];
-        // 整句插入语句拼接
-        NSString *insertStr = [NSString string];
-        insertStr = [insertStr stringByAppendingFormat:@"%@%@%@%@%@",insertStrHeader,insertStrMiddleOne,insertStrMiddleTwo,insertStrMiddleThree,insertStrTail];
-        NSMutableArray *modelPropertyArray = [NSMutableArray array];
-        for (int i = 0; i < KMODEL_PROPERTYS_COUNT; i++){
-            NSString *str = [model valueForKey:[KMODEL_PROPERTYS objectAtIndex:i]];
-            if (str == nil){
-                str = @"none";
-            }
-            [modelPropertyArray addObject: str];
-        }
-        
-        [_database executeUpdate:insertStr withArgumentsInArray:modelPropertyArray];
-    }
-    //    关闭数据库
-    [_database close];
-}
-
-/** 根据键值对删除数据 */
-- (void)deleteModelInDatabase:(id)model andWithSuffix:(NSString *)suffix andKey:(NSString *)key andValue:(NSString *)value
-{
-    // 判断是否创建数据库
-    if (!_database){
-        [self creatDatabase];
-    }
-    // 判断数据是否已经打开
-    if (![_database open]){
-        NSLog(@"数据库打开失败");
-        return;
-    }
-    // 设置数据库缓存，优点：高效
-    [_database setShouldCacheStatements:YES];
-    
-    // 判断是否有该表
-    NSString *tableName = suffix?[KCLASS_NAME(model) stringByAppendingString:suffix] : KCLASS_NAME(model);
-    if(![_database tableExists:tableName]){
-        return;
-    }
-    // 删除操作
-    // 拼接删除语句
-    // delete from tableName where userId = ?
-    
-    NSString *deletStr = [NSString stringWithFormat:@"delete from %@ where %@ = ?",tableName,key];
-    [_database executeUpdate:deletStr, value];
-    
-    // 关闭数据库
-    [_database close];
-}
-
-/** 删除表 */
-- (void)deleteTable:(id)model andWithSuffix:(NSString *)suffix
-{
-    if (!_database){
-        [self creatDatabase];
-    }
-    
-    if (![_database open]) {
-        NSLog(@"数据库打开失败");
-        return ;
-    }
-    NSString *tableName = suffix?[KCLASS_NAME(model) stringByAppendingString:suffix] : KCLASS_NAME(model);
-    NSString *seletTabelStr = [NSString stringWithFormat:@"drop table %@",tableName];
-    [_database executeUpdate:seletTabelStr];
-    //关闭数据库
-    [_database close];
-    
-}
-
 /** 查询表中所有数据 */
-- (NSArray *)selectModelArrayInDatabase:(id)model andWithSuffix:(NSString *)suffix
++ (void)queryModel:(id)model andWithSuffix:(NSString *)suffix completed:(void (^)(NSArray *))completed
 {
-    //    select * from tableName
-    if (!_database){
-        [self creatDatabase];
-    }
-    
-    if (![_database open]) {
-        NSLog(@"数据库打开失败");
-        return nil;
-    }
-    // 设置数据库缓存，优点：高效
-    [_database setShouldCacheStatements:YES];
-    //判断是否存在该表，不存在就创建
-    NSString *tableName = suffix?[KCLASS_NAME(model) stringByAppendingString:suffix] : KCLASS_NAME(model);
-    if(![_database tableExists:tableName]){
-        [self creatTable:model andWithSuffix:suffix];
-    }
-    //定义一个可变数组，用来存放查询的结果，返回给调用者
-    NSMutableArray *userModelArray = [NSMutableArray array];
-    //定义一个结果集，存放查询的数据
-    //拼接查询语句
-    NSString *selectStr = [NSString stringWithFormat:@"select * from %@",tableName];
-    FMResultSet *resultSet = [_database executeQuery:selectStr];
-    //判断结果集中是否有数据，如果有则取出数据
-    while ([resultSet next]){
-        // 用id类型变量的类去创建对象
-        id modelResult = [[[model class] alloc] init];
-        for (int i = 0; i < KMODEL_PROPERTYS_COUNT; i++){
-            [modelResult setValue:[resultSet stringForColumn:[KMODEL_PROPERTYS objectAtIndex:i]] forKey:[KMODEL_PROPERTYS objectAtIndex:i]];
+    [self queryModel:model andWithSuffix:suffix key:nil value:nil completed:^(NSArray *result) {
+        if (completed) {
+            completed(result);
         }
-        //将查询到的数据放入数组中。
-        [userModelArray addObject:modelResult];
-    }
-    // 关闭数据库
-    [_database close];
-    return userModelArray;
+    }];
 }
-#endif
+
+/** 查询表中字段等于某个值的所有记录 */
++ (void)queryModel:(id)model andWithSuffix:(NSString *)suffix key:(NSString *)key value:(NSString *)value completed:(void (^)(NSArray *))completed
+{
+    //定义一个可变数组，用来存放查询的结果，返回给调用者
+    NSMutableArray *result = [NSMutableArray array];
+    FMDBManager *dbManager = [FMDBManager sharedInstance];
+    [dbManager.databaseQueue inDatabase:^(FMDatabase *db) {
+        
+        NSString *tableName = suffix?[KCLASS_NAME(model) stringByAppendingString:suffix] : KCLASS_NAME(model);
+        if ([db tableExists:tableName])
+        {
+            //定义一个结果集，存放查询的数据
+            //拼接查询语句
+            NSString *selectStr = @"";
+            if (key && value) {
+                selectStr = [NSString stringWithFormat:@"SELECT * FROM %@ WHERE %@ = %@",tableName, key, value];
+            } else {
+                selectStr = [NSString stringWithFormat:@"SELECT * FROM %@",tableName];
+            }
+            FMResultSet *resultSet = [db executeQuery:selectStr];
+            //判断结果集中是否有数据，如果有则取出数据
+            while ([resultSet next])
+            {
+                // 用id类型变量的类去创建对象
+                id modelResult = [[[model class] alloc] init];
+                for (int i = 0; i < KMODEL_PROPERTYS_COUNT; i++)
+                {
+                    [modelResult setValue:[resultSet stringForColumn:[KMODEL_PROPERTYS objectAtIndex:i]] forKey:[KMODEL_PROPERTYS objectAtIndex:i]];
+                }
+                //将查询到的数据放入数组中。
+                [result addObject:modelResult];
+            }
+            if (completed) {
+                completed(result);
+            }
+        }
+        else
+        {
+            if (completed) {
+                completed(nil);
+            }
+            NSLog(@"%@ 表格不存在",tableName);
+        }
+    }];
+}
+
++ (void)addField:(id)model andWithSuffix:(NSString *)suffix field:(NSString *)field
+{
+    FMDBManager *dbManager = [FMDBManager sharedInstance];
+    [dbManager.databaseQueue inDatabase:^(FMDatabase *db) {
+        
+        NSString *tableName = suffix?[KCLASS_NAME(model) stringByAppendingString:suffix] : KCLASS_NAME(model);
+        if ([db tableExists:tableName])
+        {
+            // 判断字段是否存在
+            if (![db columnExists:field inTableWithName:tableName]){
+                NSString *alertStr = [NSString stringWithFormat:@"ALTER TABLE %@ ADD %@ INTEGER",tableName,field];
+                if([db executeUpdate:alertStr]){
+                    NSLog(@"%@ 字段插入成功", field);
+                }else{
+                    NSLog(@"%@ 字段插入失败", field);
+                }
+            }
+        }
+        else
+        {
+            NSLog(@"%@ 表格不存在",tableName);
+        }
+    }];
+    
+}
 
 @end
